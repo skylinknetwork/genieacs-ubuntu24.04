a8dedcdd9d42e4a7c4135cb83b173d7a8bb50607
skylinknetwork <myskylinknetwork@gmail.com>
2025-12-02 15:25:48 +0700

    Update rubah-ip.sh

--- rubah-ip.sh
+++ rubah-ip.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-
-read -p "Masukkan IP statis untuk ens33 (tanpa /24, contoh 10.20.20.7): " IPADDR
-
 cat << EOF | sudo tee /etc/netplan/00-installer-config.yaml > /dev/null
 network:
   version: 2
@@ -10,7 +6,7 @@ network:
     ens33:
       dhcp4: false
       addresses:
-        - ${IPADDR}/24
+        - 10.20.20.6/24
       routes:
         - to: default
           via: 10.20.20.1
@@ -19,6 +15,4 @@ network:
           - 8.8.8.8
           - 1.1.1.1
 EOF
-
 sudo netplan apply
-echo "Konfigurasi netplan sudah diterapkan dengan IP ${IPADDR}/24"
