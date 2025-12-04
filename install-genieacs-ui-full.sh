db0baed9945c681e60727d9207f35ab9e439f97c
skylinknetwork <myskylinknetwork@gmail.com>
2025-12-05 00:19:54 +0700

    Comment out user drop command in install script
    Comment out the command to drop users from the database.

--- install-genieacs-ui-full.sh
+++ install-genieacs-ui-full.sh
@@ -5,7 +5,7 @@ set -e
 
 echo "=== [1/5] Download file UI full dari GitHub ==="
 curl -fsSL -o /tmp/genieacs-ui-full.tar.gz \
-  https://raw.githubusercontent.com/skylinknetwork/genieacs-ubuntu24.04/main/genieacs.tar.gz
+  https://raw.githubusercontent.com/skylinknetwork/genieacs-ubuntu24.04/main/genieacs-ui-full.tar.gz
 
 echo "=== [2/5] Extract file ==="
 rm -rf /tmp/genieacs-ui-full
@@ -16,7 +16,7 @@ mongosh genieacs --eval "db.config.drop()"
 mongosh genieacs --eval "db.provisions.drop()"
 mongosh genieacs --eval "db.virtualParameters.drop()"
 mongosh genieacs --eval "db.presets.drop()"
-mongosh genieacs --eval "db.users.drop()"
+#mongosh genieacs --eval "db.users.drop()"
 
 echo "=== [4/5] Restore koleksi baru ==="
 mongorestore --db genieacs /tmp/genieacs-ui-full/genieacs
